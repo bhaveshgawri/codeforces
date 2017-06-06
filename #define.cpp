86f5fc71a3d83c1aa605130e0b82c4281ef967da
Bhavesh Gawri <4bhaveshgawri@gmail.com>
2017-06-06 20:29:37 +0530

    definations file added

---
+++ #define.cpp
@@ -0,0 +1,49 @@
+#include <btis/stdc++.h>
+
+#define ll 		long long int
+#define u64 	unsigned long long int
+
+#define nl 		"\n"
+#define pb 		push_back
+#define ppb 	pop_back
+#define pf 		push_front
+#define ppf 	pop_front
+#define sz(x) 	x.size()
+#define all(x) 	x.begin(), x.end()
+#define nfs 	ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+#define _max(a, b, c)	max(a, max(b, c))
+#define _min(a, b, c)	min(a, min(b, c))
+#define mxe(a)			*max_element(all(a))
+#define mne(a)			*min_element(all(a))
+
+#define lb(x, k) lower_bound(all(x), k)-x.begin()
+#define ub(x, k) upper_bound(all(x), k)-x.begin()
+
+#define imx INT_MAX
+#define imn INT_MIN
+#define lmx LLONG_MAX
+#define lmn LLONG_MIN
+
+#define f 	first
+#define s 	second
+
+typedef pair<int, int> ii;
+typedef pair<ll, ll> II;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+
+const int MAX = 1e5 + 4;
+const int MOD = 1e9 + 7;
+const int inf = (1<<31)-1;
+const ll INF = (1<<63)-1ll;
+
+using namespace std;
+
+int main(){
+	nfs;
+
+}
